83a3073d2f0b23da096c04b6106ddb9c6fbf2af4
Lymanto <lymantohadibrata@gmail.com>
2024-05-20 00:03:24 +0700

    update copywritting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css';
 import { Poppins } from 'next/font/google';
 
@@ -11,6 +12,12 @@ const poppins = Poppins({
 export const metadata = {
   title: 'Lymanto Hadibrata',
   description: 'This my personal website and portofolio by Lymanto Hadibrata',
+  openGraph: {
+    title: 'Lymanto Hadibrata',
+    description: 'This my personal website and portofolio by Lymanto Hadibrata',
+    type: 'website',
+    url: 'lymanto.vercel.app',
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +27,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body className={`${poppins.className} text-white bg-primary `}>
         {children}
       </body>
